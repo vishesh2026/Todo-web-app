94de2d9c2808d7f23801eeefeb62e62e7e8feaea
Vishesh Kumawat <visheshunique2004@gmail.com>
2026-01-30 14:19:32 +0530

    fix: update axios baseURL to deployed backend

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api"
+    baseURL: process.env.REACT_APP_API_BASE_URL || "https://todo-web-app-750s.onrender.com/api"
 });
 
 export default instance;
